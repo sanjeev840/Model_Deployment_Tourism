ebb756c584a6e07282424783c0f411520be4602f
Sanjeev Kumar Choudhary <95634421+sanjeev840@users.noreply.github.com>
2026-08-16 18:33:55 +0530

    update tourism_project/deployment/app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import pandas as pd
 import joblib
-4from pathlib import Path
+from pathlib import Path
 
 # Load the model committed by the pipeline (sits next to this file)
 #model_path = os.path.join(os.path.dirname(__file__), "tourism_package_prediction_model_v1.joblib")
